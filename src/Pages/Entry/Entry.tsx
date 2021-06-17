04003df580ebf699ac824d6065e2224877c1a6bb
Lauviah Fang <lauviah0622@gmail.com>
2021-06-17 17:55:39 +0800

    add: error message and differnt button state

--- src/Pages/Entry/Entry.tsx
+++ src/Pages/Entry/Entry.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import styled from "styled-components";
-import { useHistory, RouteComponentProps } from "react-router-dom";
+import { useHistory, useLocation, RouteComponentProps } from "react-router-dom";
 
 import { Button, WhiteSpace, WingBlank, List, InputItem } from "antd-mobile";
 
@@ -8,15 +8,22 @@ import Layout from "../../Components/Layout";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../redux/store";
 import {
-  createSetSpreadSheetState,
+  createSetIdAction,
   spreadSheetStateSelector,
   SpreadSheetState,
 } from "../../redux/feature/spreadSheet/spreadSheetSlice";
+import useSignHook from "../../hooks/useSign";
+import useUrlInputState from './useUrlInputState';
+import useSetSheetFields from "../../hooks/useSetSheetFields";
 
 const Title = styled.h3``;
 
 const PageWrapper = styled.div`
   height: 100vh;
+`;
+
+const ErrorMessage = styled.p`
+  color: red;
 `;
 
 const ContentWrapper = styled.div`
@@ -26,44 +33,141 @@ const ContentWrapper = styled.div`
 
 interface EntryProps extends RouteComponentProps<{}> {}
 
-export default function Entry(props: EntryProps) {
-  const [spreadSheetUrl, setSpreadSheetUrl] = useState<string>("");
-  const { headerRange, sheetId }: SpreadSheetState = useSelector(
-    spreadSheetStateSelector
-  );
+function useTransUrl(pathname: string, history: any) {
+  useEffect(() => {
+    const sheetIDMatch = location.pathname.match(/\/([\w\-]{40,})$/);
+    if (sheetIDMatch) {
+      return;
+    }
+    const sheetUrlMatch = location.pathname.match(
+      /docs\.google\.com\/spreadsheets\/d\/([\w\-]{40,})\//
+    );
+    if (sheetUrlMatch) {
+      console.log("url");
+      history.push(`/${sheetUrlMatch[1]}`);
+      return;
+    }
+    history.push(`/`);
+  }, [location, history]);
+}
+/* 
+function useUpdateIdByUrl(pathname: string, dispatch: any) {
+  useEffect(() => {
+    const sheetIDMatch = location.pathname.match(/\/([\w\-]{40,})$/);
+    if (sheetIDMatch) {
+      console.log("only Id");
+      dispatch(createSetIdAction(sheetIDMatch[1]));
+      return;
+    }
+  });
+}
+ */
 
-  const dispatch: AppDispatch = useDispatch();
-  const history = useHistory();
+const extractSpreadIdFrom = (str: string): string | null => {
+  const urlMatchIdResult = str.match(
+    /docs\.google\.com\/spreadsheets\/d\/([\w\-]{40,})\//
+  );
+  return urlMatchIdResult ? urlMatchIdResult[1] : null;
+};
+
+interface EntryStates {
+  isPending: boolean;
+  isSpreadSheetUrlValid: boolean;
+  isSignIn: boolean;
+}
+
+// TODO: 這裡的判斷可以做簡化，isSpreadSheetUrlValid 基本上就是控制 disable
+const getButtonByStates = (states: EntryStates): string | JSX.Element => {
+  const { isPending, isSpreadSheetUrlValid, isSignIn } = states;
+  if (isPending)
+    return (
+      <Button type="primary" loading disabled>
+        Loging...
+      </Button>
+    );
+
+  switch (true) {
+    case isSpreadSheetUrlValid && isSignIn:
+      return <Button type="primary">已登入，下一步</Button>;
+
+    case isSpreadSheetUrlValid && !isSignIn:
+      return <Button type="primary">登入 google 帳戶</Button>;
+
+    case !isSpreadSheetUrlValid && isSignIn:
+      return (
+        <Button type="primary" disabled>已登入，下一步</Button>
+      );
+
+    case !isSpreadSheetUrlValid && !isSignIn:
+      return (
+        <Button type="primary" disabled>登入</Button>
+      );
+    default:
+      break;
+  }
+  // ? 基本上不會到這個，但這裡要怎麼做錯誤處理？
+  return <Button type="warning" disabled>Error</Button>;
+};
+
+export default function Entry(props: EntryProps) {
+  const [isPending, setIsPending] = useState<EntryStates["isPending"]>(false);
+  const {
+    isDirty,
+    spreadSheetUrl,
+    spreadSheetId,
+    setSpreadSheetUrl
+  } = useUrlInputState()
+  
+  const location = useLocation();
+
+  //* 根據 url update Input 裡面的內容
+  useEffect(() => {
+    function updateIdByPathName(pathName: string) {
+      const spreadSheetUrlMatchArray = pathName.match(/[^\/].*/g);
+      if (spreadSheetUrlMatchArray) {
+        setSpreadSheetUrl(spreadSheetUrlMatchArray[0]);
+      }
+    }
+    updateIdByPathName(location.pathname);
+  }, [location]);
 
   // 在 React 裡面要這樣用
   const inputChangeHandler = (value: string) => {
     setSpreadSheetUrl(value);
   };
 
-  const submitBtnHandler = () => {
-    history.push(`/${spreadSheetUrl}`);
+  // SignIn
+  const [isSignIn, signIn] = useSignHook();
+  const loginHandler = () => {
+    signIn();
   };
+
+  const Button = getButtonByStates({
+    isPending,
+    isSignIn: true,
+    isSpreadSheetUrlValid: !!spreadSheetId,
+  });
+
+  const errorMessage = isDirty && !spreadSheetId && 'Eroor';
   return (
     <PageWrapper>
-      <Layout
-        footer={
-          <Button type="primary" onClick={submitBtnHandler}>
-            完成
-          </Button>
-        }
-      >
+      <Layout footer={Button}>
         <ContentWrapper>
-            <WingBlank>
-              <Title>Google SpreadSheet</Title>
-            </WingBlank>
-            <WingBlank>
-              <InputItem
-                value={spreadSheetUrl}
-                placeholder="輸入您的 Google sheet 網址"
-                onChange={inputChangeHandler}
-              />
-            </WingBlank>
-            <WhiteSpace size="xl" />
+          <WingBlank>
+            <Title>Google SpreadSheet</Title>
+          </WingBlank>
+          <WingBlank>
+            <InputItem
+              value={spreadSheetUrl}
+              placeholder="輸入您的 Google sheet 網址以獲得 id"
+              onChange={inputChangeHandler}
+              disabled={isPending}
+            />
+            <ErrorMessage>{errorMessage}</ErrorMessage>
+            <p>isDirty: {isDirty + ""}</p>
+            <p>isSpreadSheetUrlValid: {spreadSheetId + ""}</p>
+          </WingBlank>
+          <WhiteSpace size="xl" />
         </ContentWrapper>
       </Layout>
     </PageWrapper>
---
+++ src/Pages/Entry/useUrlInputState.ts
@@ -0,0 +1,79 @@
+import { useReducer, useMemo } from "react";
+
+const TYPE = {
+  updateSpreadSheelUrl: "spreadSheetUrl.update",
+  updateVarifiedSpreadSheetId: "varificatedSpreadSheetId.update"
+} as const;
+
+type SpreadSheetUrlInputState = {
+  isDirty: boolean;
+  spreadSheetUrl: string;
+};
+
+type ActionType = { type: typeof TYPE.updateSpreadSheelUrl; payload: string }
+
+
+
+function UrlInputReducer(
+  state: SpreadSheetUrlInputState,
+  { type, payload }: ActionType
+): SpreadSheetUrlInputState {
+  switch (type) {
+    case TYPE.updateSpreadSheelUrl:
+      {
+
+        return {
+          ...state,
+          spreadSheetUrl: payload,
+          isDirty: true,
+        };
+
+      }
+
+    default:
+      throw new Error();
+  }
+}
+
+const URL_MATCH_REGEX = /docs\.google\.com\/spreadsheets\/d\/([\w\-]{40,})\//;
+const extractSpreadIdFrom = (str: string): string | null => {
+  const urlMatchIdResult = str.match(URL_MATCH_REGEX);
+  return urlMatchIdResult ? urlMatchIdResult[1] : null;
+};
+
+function asnycVerified(spreadSheetUrl:string):Promise<boolean> {
+  const res = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true)
+    }, 1000)
+  }) as Promise<true> //? 這裡怎麼辦？
+  return res
+}
+
+export default function useUrlInputState() {
+  const [state, dispatch] = useReducer<typeof UrlInputReducer>(UrlInputReducer, {
+    isDirty: false,
+    spreadSheetUrl: ''
+  })
+
+  const spreadSheetId = useMemo(
+    () => extractSpreadIdFrom(state.spreadSheetUrl),
+    [state.spreadSheetUrl]
+  );
+
+  const setSpreadSheetUrl = (SpreadSheelUrl: string) => {
+    dispatch({
+      type: TYPE.updateSpreadSheelUrl,
+      payload: SpreadSheelUrl
+    })
+  }
+
+
+  return {
+    spreadSheetId,
+    spreadSheetUrl: state.spreadSheetUrl,
+    isDirty: state.isDirty,
+    setSpreadSheetUrl
+  }
+
+}
